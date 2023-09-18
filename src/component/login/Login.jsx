7a6b43b26c40cb6ed3e4c1eaa74ec5b4a0eafa98
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-09-18 18:37:02 +0600

    login and register setup complete

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,50 +1,4 @@
-// import React, { useContext } from 'react';
-// import { Link } from 'react-router-dom';
-// import { AuthContext } from '../../providers/AuthProviders';
 
-// const Login = () => {
-//     const {signIn} = useContext(AuthContext);
-//     const handleLogin = (event) => {
-//         event.preventDefault();
-//         const form = event.target;
-//         const email = form.email.value;
-//         const password = form.password.value;
-//         console.log(email, password);
-
-//         signIn(email,password)
-//         .then(result =>{
-//           const loggedUser = result.user;
-//           console.log(loggedUser);
-//         })
-//         .catch(error =>{
-//             console.log(error);
-//         })
-//     }
-//     return (
-//         <form onSubmit={handleLogin} className='m-auto  w-2/4'>
-//             <div className='p-10 w-3/4 border-2 m-auto'>
-//                 <h3 className='text-center text-xl'>Please Login</h3>
-//                 <div>
-//                     <label className='block text-xl'>Email</label>
-//                     <input className='w-full border-2 py-2 bg-lime-100 border-lime-600' type="email" name="email" placeholder='Enter Your Email' id="" />
-//                 </div>
-//                 <div>
-//                     <label className='block text-xl'>Password</label>
-//                     <input className=' w-full border-2 py-2 bg-lime-100 border-lime-600' type="text" name="password" placeholder='Enter Your password' id="" />
-//                 </div>
-//                 <div>
-//                     <p>new to ema-john?<span className='text-red-500'><Link to='/register'>Create an Account</Link></span></p>
-//                 </div>
-//                 <div>
-//                     <button className='w-full border-2 py-2 mt-5 bg-lime-600 border-lime-600 text-white' type="submit">Login</button>
-//                 </div>
-//             </div>
-//         </form>
-//     );
-// };
-
-// export default Login;
-//*******************************************/
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProviders';
@@ -70,19 +24,19 @@ const Login = () => {
            console.log(error);
         })
     }
-    const handleGoogle = () =>{
-        handleGoogleSignIn()
-        .then(result=>{
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(error=>{
-           console.log(error);
-        })
-    }
+    // const handleGoogle = () =>{
+    //     handleGoogleSignIn()
+    //     .then(result=>{
+    //         const user = result.user;
+    //         console.log(user);
+    //     })
+    //     .catch(error=>{
+    //        console.log(error);
+    //     })
+    // }
 
     return (
-         <form onSubmit={handleLogin} className='m-auto  w-2/4'>
+         <form onSubmit={handleLogin} className='m-auto mt-5  w-2/4'>
             <div className='p-10 w-3/4 border-2 m-auto'>
                 <h3 className='text-center text-xl'>Please Login</h3>
                 <div>
--- src/component/register/Register.jsx
+++ src/component/register/Register.jsx
@@ -6,7 +6,7 @@
 //     const {createUser , user} = useContext(AuthContext);
 //     console.log(user);
 //     const [error,setError] = useState('');
-    
+
 //     const handleRegister = (event) =>{
 //         event.preventDefault();
 //         const form = event.target;
@@ -64,12 +64,13 @@
 
 // export default Register;
 //******************************************************/
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProviders';
 
 const Register = () => {
-    const {createUser,user} = useContext(AuthContext);
+    const { createUser, user } = useContext(AuthContext);
+    const {error,setError} = useState('')
     console.log(user);
     const handleRegister = event => {
         event.preventDefault();
@@ -77,43 +78,52 @@ const Register = () => {
         const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        // console.log(name,email, password);
+        console.log(name, email, password);
+        if (password !== confirmPassword) {
+            setError('Your password did not match')
+            return
+        }
 
-        createUser(email,password)
-        .then(result =>{
-            const loggedUser = result.user;
-            console.log(loggedUser);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+        if (password.length < 6) {
+            setError('Password must be 6 character')
+            return
+        }
+
+        createUser(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+            })
+            .catch(error => {
+                console.log(error);
+            })
 
     }
     return (
-         <form onSubmit={handleRegister} className='m-auto mt-5 w-2/4'>
-        <div className='p-10 w-3/4 border-2 m-auto'>
-            <h3 className='text-center text-xl'>Please Register</h3>
-            <div  className="form-control">
-                <label className='block text-xl'>Email</label>
-                <input className='rounded w-full border-2 py-2 bg-lime-100 border-lime-600' type="email" name="email" placeholder='Enter Your Email' id="" />
+        <form onSubmit={handleRegister} className='m-auto mt-5 w-2/4'>
+            <div className='p-10 w-3/4 border-2 m-auto'>
+                <h3 className='text-center text-xl'>Please Register</h3>
+                <div className="form-control">
+                    <label className='block text-xl'>Email</label>
+                    <input className='rounded w-full border-2 py-2 bg-lime-100 border-lime-600' type="email" name="email" placeholder='Enter Your Email' id="" />
+                </div>
+                <div className="form-control">
+                    <label className='block text-xl'>Password</label>
+                    <input className='rounded w-full border-2 py-2 bg-lime-100 border-lime-600' type="text" name="password" placeholder='Enter Your password' id="" />
+                </div>
+                <div className="form-control">
+                    <label className='block text-xl'>Confirm Password</label>
+                    <input className='rounded w-full border-2 py-2 bg-lime-100 border-lime-600' type="text" name="confirmPassword" placeholder='Enter Your password' id="" />
+                </div>
+                <div className="form-control">
+                    <p className=''><Link to='/login'>Already have an Account?</Link></p>
+                </div>
+                <div>
+                    <button className='rounded w-full border-2 py-2 mt-5 bg-lime-600 border-lime-600 text-white' type="submit">Register</button>
+                </div>
+                <p>{error}</p>
             </div>
-            <div className="form-control">
-                <label className='block text-xl'>Password</label>
-                <input className='rounded w-full border-2 py-2 bg-lime-100 border-lime-600' type="text" name="password" placeholder='Enter Your password' id="" />
-            </div>
-            <div className="form-control">
-                <label className='block text-xl'>Confirm Password</label>
-                <input className='rounded w-full border-2 py-2 bg-lime-100 border-lime-600' type="text" name="confirmPassword" placeholder='Enter Your password' id="" />
-            </div>
-            <div className="form-control">
-                <p className=''><Link to='/login'>Already have an Account?</Link></p>
-            </div>
-            <div>
-                <button className='rounded w-full border-2 py-2 mt-5 bg-lime-600 border-lime-600 text-white' type="submit">Register</button>
-            </div>
-            {/* <p>{error}</p> */}
-        </div>
-    </form>
+        </form>
     );
 };
 
